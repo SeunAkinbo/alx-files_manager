dcf85e4b971663a95e7cae28879e9154ba7eac46
Oluwaseun Akinbo <seunakinbo@gmail.com>
2024-09-03 19:27:33 +0100

    First API

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -1,26 +1,18 @@
-// controllers/AppController.js
-
-const redisClient = require("../utils/redis");
-const dbClient = require("../utils/db");
+import redisClient from "../utils/redis";
+import dbClient from "../utils/db";
 
 class AppController {
-  // GET /status endpoint
   static async getStatus(req, res) {
-    const status = {
-      redis: redisClient.isAlive(),
-      db: await dbClient.isAlive(),
-    };
-    res.status(200).json(status);
+    const redisStatus = redisClient.isAlive();
+    const dbStatus = dbClient.isAlive();
+    return res.status(200).json({ redis: redisStatus, db: dbStatus });
   }
 
-  // GET /stats endpoint
   static async getStats(req, res) {
-    const stats = {
-      users: await dbClient.nbUsers(),
-      files: await dbClient.nbFiles(),
-    };
-    res.status(200).json(stats);
+    const usersCount = await dbClient.nbUsers();
+    const filesCount = await dbClient.nbFiles();
+    return res.status(200).json({ users: usersCount, files: filesCount });
   }
 }
 
-module.exports = AppController;
+export default AppController;
--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,25 @@
-// routes/index.js
-
-const express = require("express");
-const AppController = require("../controllers/AppController");
+import express from "express";
+import AppController from "../controllers/AppController";
+import UsersController from "../controllers/UsersController";
+import AuthController from "../controllers/AuthController";
+import FilesController from "../controllers/FilesController";
 
 const router = express.Router();
 
-// Define the endpoints and map them to the corresponding controller methods
 router.get("/status", AppController.getStatus);
 router.get("/stats", AppController.getStats);
 
-module.exports = router;
+router.post("/users", UsersController.postNew);
+router.get("/users/me", UsersController.getMe);
+
+router.get("/connect", AuthController.getConnect);
+router.get("/disconnect", AuthController.getDisconnect);
+
+router.post("/files", FilesController.postUpload);
+router.get("/files/:id", FilesController.getShow);
+router.get("/files", FilesController.getIndex);
+router.put("/files/:id/publish", FilesController.putPublish);
+router.put("/files/:id/unpublish", FilesController.putUnpublish);
+router.get("/files/:id/data", FilesController.getFile);
+
+export default router;
